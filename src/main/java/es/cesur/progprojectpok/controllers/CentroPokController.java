aa0ddae85a12cfd4082d4091b9c838454392e3af
DanielPalazónCastillo <dpalazon59@gmail.com>
2024-04-19 14:01:30 +0200

    Commit todo lo hecho en Daniel-Branch original

---
+++ src/main/java/es/cesur/progprojectpok/controllers/CentroPokController.java
@@ -0,0 +1,95 @@
+package es.cesur.progprojectpok.controllers;
+
+import es.cesur.progprojectpok.ImageData;
+import es.cesur.progprojectpok.utils.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CentroPokController implements Initializable {
+
+    @FXML
+    private ImageView imgPok1;
+
+    @FXML
+    private ImageView imgPok2;
+
+    @FXML
+    private ImageView imgPok3;
+
+    @FXML
+    private ImageView imgPok4;
+
+    @FXML
+    private ImageView imgPok5;
+
+    @FXML
+    private ImageView imgPok6;
+
+    @FXML
+    private Label lblPok1;
+
+    @FXML
+    private Label lblPok2;
+
+    @FXML
+    private Label lblPok3;
+
+    @FXML
+    private Label lblPok4;
+
+    @FXML
+    private Label lblPok5;
+
+    @FXML
+    private Label lblPok6;
+
+    @FXML
+    private ProgressBar pgrsbar1;
+
+    @FXML
+    private ProgressBar pgrsbar2;
+
+    @FXML
+    private ProgressBar pgrsbar3;
+
+    @FXML
+    private ProgressBar pgrsbar4;
+
+    @FXML
+    private ProgressBar pgrsbar5;
+
+    @FXML
+    private ProgressBar pgrsbar6;
+
+    @FXML
+    void realizarCura(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  {@code null} if the location is not known.
+     * @param resources The resources used to localize the root object, or {@code null} if
+     *                  the root object was not localized.
+     */
+
+
+}
---
+++ src/main/java/es/cesur/progprojectpok/controllers/CombateLucharController.java
@@ -0,0 +1,86 @@
+package es.cesur.progprojectpok.controllers;
+
+import es.cesur.progprojectpok.ImageData;
+import es.cesur.progprojectpok.utils.Utils;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CombateLucharController implements Initializable {
+
+    @FXML
+    private ImageView Gender1;
+
+    @FXML
+    private ImageView Gender2;
+
+    @FXML
+    private ImageView Perso1;
+
+    @FXML
+    private ImageView Perso2;
+
+    @FXML
+    private ImageView Pok1;
+
+    @FXML
+    private ImageView Pok2;
+
+    @FXML
+    private ImageView Pok3;
+
+    @FXML
+    private ImageView Pok4;
+
+    @FXML
+    private ImageView Pok5;
+
+    @FXML
+    private ImageView Pok6;
+
+    @FXML
+    private Button btn1;
+
+    @FXML
+    private Button btn2;
+
+    @FXML
+    private Button btn3;
+
+    @FXML
+    private Button btn4;
+
+    @FXML
+    private Label lblComb1;
+
+    @FXML
+    private Label lblNombre1;
+
+    @FXML
+    private Label lblNombre2;
+
+    @FXML
+    private Label lblNvl1;
+
+    @FXML
+    private Label lblNvl2;
+
+    @FXML
+    private ProgressBar prgrsBar1;
+
+    @FXML
+    private ProgressBar prgrsBar2;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
