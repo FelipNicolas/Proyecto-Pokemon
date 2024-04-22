0fce45a88059782d377d16cf4aad9ea74fae5b30
DanielPalazónCastillo <dpalazon59@gmail.com>
2024-04-22 13:15:57 +0200

    Commit de views de combate más sus Controller

---
+++ src/main/java/es/cesur/progprojectpok/controllers/CombateEquipoController.java
@@ -0,0 +1,126 @@
+package es.cesur.progprojectpok.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CombateEquipoController implements Initializable {
+
+    @FXML
+    private ImageView Gender1;
+
+    @FXML
+    private ImageView Gender2;
+
+    @FXML
+    private ImageView Perso1;
+
+    @FXML
+    private ImageView Perso2;
+
+    @FXML
+    private ImageView Pok1;
+
+    @FXML
+    private ImageView Pok2;
+
+    @FXML
+    private ImageView Pok3;
+
+    @FXML
+    private ImageView Pok4;
+
+    @FXML
+    private ImageView Pok5;
+
+    @FXML
+    private ImageView Pok6;
+
+    @FXML
+    private Button btnPK1;
+
+    @FXML
+    private Button btnPK2;
+
+    @FXML
+    private Button btnPK32;
+
+    @FXML
+    private Button btnPK4;
+
+    @FXML
+    private Button btnPK5;
+
+    @FXML
+    private Button btnPK6;
+
+    @FXML
+    private ImageView img1;
+
+    @FXML
+    private ImageView img10;
+
+    @FXML
+    private ImageView img11;
+
+    @FXML
+    private ImageView img12;
+
+    @FXML
+    private ImageView img2;
+
+    @FXML
+    private ImageView img3;
+
+    @FXML
+    private ImageView img4;
+
+    @FXML
+    private ImageView img5;
+
+    @FXML
+    private ImageView img6;
+
+    @FXML
+    private ImageView img7;
+
+    @FXML
+    private ImageView img8;
+
+    @FXML
+    private ImageView img9;
+
+    @FXML
+    private Label lblComb1;
+
+    @FXML
+    private Label lblNombre1;
+
+    @FXML
+    private Label lblNombre2;
+
+    @FXML
+    private Label lblNvl1;
+
+    @FXML
+    private Label lblNvl2;
+
+    @FXML
+    private ProgressBar prgrsBar1;
+
+    @FXML
+    private ProgressBar prgrsBar2;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ src/main/java/es/cesur/progprojectpok/controllers/CombateMochilaController.java
@@ -0,0 +1,110 @@
+package es.cesur.progprojectpok.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CombateMochilaController implements Initializable {
+
+    @FXML
+    private ImageView Gender1;
+
+    @FXML
+    private ImageView Gender2;
+
+    @FXML
+    private ImageView Perso1;
+
+    @FXML
+    private ImageView Perso2;
+
+    @FXML
+    private ImageView Pok1;
+
+    @FXML
+    private ImageView Pok2;
+
+    @FXML
+    private ImageView Pok3;
+
+    @FXML
+    private ImageView Pok4;
+
+    @FXML
+    private ImageView Pok5;
+
+    @FXML
+    private ImageView Pok6;
+
+    @FXML
+    private Button btnBACK;
+
+    @FXML
+    private Button btnOBJ1;
+
+    @FXML
+    private Button btnOBJ2;
+
+    @FXML
+    private Button btnOBJ3;
+
+    @FXML
+    private Button btnOBJ4;
+
+    @FXML
+    private Button btnOBJ5;
+
+    @FXML
+    private Button btnOBJ6;
+
+    @FXML
+    private ImageView img1;
+
+    @FXML
+    private ImageView img2;
+
+    @FXML
+    private ImageView img3;
+
+    @FXML
+    private ImageView img4;
+
+    @FXML
+    private ImageView img5;
+
+    @FXML
+    private ImageView img6;
+
+    @FXML
+    private Label lblComb1;
+
+    @FXML
+    private Label lblNombre1;
+
+    @FXML
+    private Label lblNombre2;
+
+    @FXML
+    private Label lblNvl1;
+
+    @FXML
+    private Label lblNvl2;
+
+    @FXML
+    private ProgressBar prgrsBar1;
+
+    @FXML
+    private ProgressBar prgrsBar2;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
